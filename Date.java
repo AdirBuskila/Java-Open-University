0285e5e202b05af4d85db91d8e3a15dc34c8082b
Adir Buskila <Adirbu98@gmail.com>
2022-11-15 19:18:11 +0200

    comments

--- Date.java
+++ Date.java
@@ -41,15 +41,18 @@ public class Date {
     }
 
     public boolean before(Date other) {
-        if (_year < other.getYear() || _year == other.getYear() && _month < other.getMonth()
-                || _year == other.getYear() && _month == other.getMonth() && _day < other.getDay()) {
-            return true;
-        }
-        return false;
-    }
+        // if the _year is less than other's year return true
+        // if the _year is equal to other's year and _month is less than other's month
+        // return true
+        // if the _year is equal to other's year and _month is equal to other's month
+        // and day is less than other's day return true
+        // else return false
+        return(_year < other.getYear() || _year == other.getYear() && _month < other.getMonth()
+                || _year == other.getYear() && _month == other.getMonth() && _day < other.getDay());
+    } 
 
     public String toString() {
-        String dayString = (_day > MAX_ONE_DIGIT_NUMBER) ? _day + "" : "0" + _day;
+        String dayString = (_day > MAX_ONE_DIGIT_NUMBER) ? _day + "" : "0" + _day; // checking if number is above 9
         String monthString = (_month > MAX_ONE_DIGIT_NUMBER) ? _month + "" : "0" + _month;
         return dayString + "/" + monthString + "/" + _year;
     }
