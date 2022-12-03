de88f1b4ded489c86d9f52eb6639df454b33154c
Adir Buskila <Adirbu98@gmail.com>
2022-12-03 22:20:04 +0200

    testers init

---
+++ AdirCarTest.java
@@ -0,0 +1,122 @@
+public class AdirCarTest {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+
+    private static int testNumber = 0;
+
+    public static void main(String[] args) {
+        System.out.println("---Car Tester---");
+        Car c1 = new Car(0, 'A', "Ford", false);
+        Car c2 = new Car(0, 'F', "Ford", false);
+        Car c3 = new Car(1234567, 'C', "Ford", false);
+        Car c4 = new Car(0, 'A', "Ford", false);
+        Car c5 = new Car(5555555, 'B', "Ford", true);
+        Car c6 = new Car(6666666, 'C', "Ford", true);
+        Car c7 = new Car(7777777, 'F', "Ford", true);
+        Car c8 = new Car(8888888, 'A', "Ford", true);
+        Car c9 = new Car(0, 'B', "Ford", false);
+        Car c10 = new Car(0, 'D', "Ford", false);
+        Car c11 = new Car(0, 'D', "Ford", true);
+        String c1ToString = "id:1111111 type:A brand:BMW gear:auto";
+        String c2ToString = "id:9999999 type:A brand:Ford gear:auto";
+        String c3ToString = "id:1234567 type:C brand:Ford gear:auto";
+        String c4ToString = "id:9999999 type:A brand:Ford gear:auto";
+        String c5ToString = "id:5555555 type:B brand:Ford gear:manual";
+        String c6ToString = "id:6666666 type:C brand:Ford gear:manual";
+        String c7ToString = "id:7777777 type:A brand:Ford gear:manual";
+        String c8ToString = "id:8888888 type:A brand:Ford gear:manual";
+        int defaultID = 9999999;
+        char defaultType = 'A';
+        System.out.println("--Testing Constructor--");
+        test(c1.getId() == defaultID);
+        test(c1.getId() != 0);
+        test(c2.getId() == defaultID);
+        test(c2.getType() == defaultType);
+        test(c3.getId() == 1234567);
+        test(c4.getId() == defaultID);
+        System.out.println("--Testing getters--");
+        test(c1.getBrand() == "Ford");
+        test(c1.isManual() == false);
+        System.out.println("--Testing setters--");
+        System.out.println("-ID-");
+        c1.setId(1);
+        test(c1.getId() == defaultID && c1.getId() != 1);
+        c1.setId(123456789);
+        test(c1.getId() == defaultID && c1.getId() != 123456789);
+        c1.setId(1111111);
+        test(c1.getId() == 1111111);
+        System.out.println("-Type-");
+        c1.setType('Z');
+        test(c1.getType() == 'A');
+        c1.setType('B');
+        test(c1.getType() == 'B');
+        c1.setType('C');
+        test(c1.getType() == 'C');
+        c1.setType('D');
+        test(c1.getType() == 'D');
+        c1.setType('A');
+        test(c1.getType() == 'A');
+        System.out.println("-Brand-");
+        c1.setBrand("BMW");
+        test(c1.getBrand() == "BMW");
+        System.out.println("--toString--");
+        test(c1.toString().equals(c1ToString));
+        test(c2.toString().equals(c2ToString));
+        test(c3.toString().equals(c3ToString));
+        test(c4.toString().equals(c4ToString));
+        test(c5.toString().equals(c5ToString));
+        test(c6.toString().equals(c6ToString));
+        test(c7.toString().equals(c7ToString));
+        test(c8.toString().equals(c8ToString));
+        System.out.println("--Equals--");
+        test(!c1.equals(c2));
+        test(c1.equals(c1));
+        c7.setId(8888888);
+        test(c7.equals(c8));
+        System.out.println("--Better--");
+        test(c1.isBetter(c3) == false);
+        test(c1.isBetter(c1) == false);
+        test(c10.isBetter(c10) == false);
+        test(c3.isBetter(c1));
+        test(c1.isBetter(c8));
+        test(c8.isBetter(c1) == false);
+        test(c9.isBetter(c1));
+        test(c9.isBetter(c3) == false);
+        test(c9.isBetter(c10) == false);
+        test(c3.isBetter(c5));
+        test(c3.isBetter(c6));
+        test(c10.isBetter(c1));
+        test(c10.isBetter(c3));
+        test(c10.isBetter(c5));
+        test(c10.isBetter(c11));
+        System.out.println("--Worse--");
+        test(c1.worse(c3)); //
+        test(c3.worse(c1) == false);
+        test(c1.worse(c8) == false);
+        test(c8.worse(c1)); //
+        test(c9.worse(c1) == false);
+        test(c9.worse(c3)); //
+        test(c9.worse(c10)); //
+        test(c3.worse(c5) == false);
+        test(c3.worse(c6) == false);
+        test(c10.worse(c1) == false);
+        test(c10.worse(c3) == false);
+        test(c10.worse(c5) == false);
+        test(c10.worse(c11) == false);
+        System.out.println("--Checking if not returning 'true' if same Car--");
+        test(c10.worse(c10) == false);
+
+    }
+
+    private static void test(boolean testCondition) {
+        System.out.println(ANSI_BLUE + "TestNum: " + (++testNumber) + ANSI_RESET);
+        if (testCondition) {
+            System.out.println(ANSI_GREEN + "OK" + ANSI_RESET);
+        } else {
+            System.out.println(ANSI_RED + "ERROR" + ANSI_RESET);
+        }
+    }
+}
---
+++ AdirRentTest.java
@@ -0,0 +1,108 @@
+/*
+ * Created by: Adir Buskila
+ * required Classes: Date, Car, Rent
+ * if you find any bugs at the test please notify me :)
+ */
+
+public class AdirRentTest {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+
+    private static int testNumber = 0;
+
+    public static void main(String[] args) {
+        System.out.println("--Rent Tester--");
+        Date d1 = new Date(1, 1, 2000);
+        Date d2 = new Date(6, 1, 2000);
+        Date d3 = new Date(8, 1, 2000);
+        Date d4 = new Date(10, 1, 2000);
+        Date d5 = new Date(16, 1, 2000);
+        Car carA = new Car(123456789, 'A', "Ford", false);
+        Car carB = new Car(123456789, 'B', "Ford", false);
+        Car carC = new Car(123456789, 'C', "Ford", false);
+        Car carD = new Car(123456789, 'D', "Ford", false);
+        Rent r1 = new Rent("Rent 1", carA, d1, d2);
+        Rent r2 = new Rent("Rent 2", carA, d1, d3);
+        Rent r3 = new Rent("Rent 3", carA, d1, d4);
+        Rent r4 = new Rent("Rent 4", carA, d1, d1);
+        Rent r5 = new Rent("Rent 5", carA, d1, d5);
+        Rent r6 = new Rent("Rent 6", carB, d1, d5);
+        Rent r7 = new Rent("Rent 7", carC, d1, d5);
+        Rent r8 = new Rent("Rent 8", carD, d1, d5);
+        String r1ToString = "Name:Rent 1 From:01/01/2000 To:06/01/2000 Type:A Days:5 Price:500";
+        String r2ToString = "Name:Rent 2 From:01/01/2000 To:08/01/2000 Type:A Days:7 Price:630";
+        String r3ToString = "Name:Rent 3 From:01/01/2000 To:10/01/2000 Type:A Days:9 Price:830";
+        String r4ToString = "Name:Rent 4 From:01/01/2000 To:02/01/2000 Type:A Days:1 Price:100";
+        String r5ToString = "Name:Rent 5 From:01/01/2000 To:16/01/2000 Type:A Days:15 Price:1360";
+        String r6ToString = "Name:Rent 6 From:01/01/2000 To:16/01/2000 Type:B Days:15 Price:2040";
+        String r7ToString = "Name:Rent 7 From:01/01/2000 To:16/01/2000 Type:C Days:15 Price:2448";
+        String r8ToString = "Name:Rent 8 From:01/01/2000 To:16/01/2000 Type:D Days:15 Price:3264";
+
+        System.out.println("---Testing constructor---");
+        System.out.println("r4 = " + r4);
+        test(!r4.getPickupDate().equals(r4.getReturnDate()));
+        test(r4.getPickupDate().tomorrow().equals(r4.getPickupDate().tomorrow()));
+        Rent rent1 = new Rent(r1);
+        System.out.println("---Testing equals---");
+        test(r1.equals(rent1));
+        test(!r1.equals(r2));
+        System.out.println("---Testing howManyDays---");
+        test(r1.howManyDays() == 5);
+        test(r2.howManyDays() == 7);
+        test(r3.howManyDays() == 9);
+        test(r4.howManyDays() == 1);
+        System.out.println("---Testing getPrice---");
+        test(r1.getPrice() == 500);
+        test(r2.getPrice() == 630);
+        test(r3.getPrice() == 830);
+        test(r5.getPrice() == 1360);
+        System.out.println("---Testing upgrade---");
+        System.out.println("-1-");
+        test(r8.upgrade(carD) == 0);
+        test(r8.upgrade(carC) == 0);
+        test(r8.upgrade(carB) == 0);
+        test(r8.upgrade(carA) == 0);
+        System.out.println("-2-");
+        test(r7.upgrade(carD) == 816);
+        r7.setCar(carC);
+        test(r7.upgrade(carC) == 0);
+        test(r7.upgrade(carB) == 0);
+        test(r7.upgrade(carA) == 0);
+        System.out.println("-3-");
+        test(r6.upgrade(carD) == 1224);
+        r6.setCar(carB);
+        test(r6.upgrade(carC) == 408);
+        r6.setCar(carB);
+        test(r6.upgrade(carB) == 0);
+        test(r6.upgrade(carA) == 0);
+        System.out.println("-4-");
+        test(r5.upgrade(carD) == 1904);
+        r5.setCar(carA);
+        test(r5.upgrade(carC) == 1088);
+        r5.setCar(carA);
+        test(r5.upgrade(carB) == 680);
+        r5.setCar(carA);
+        test(r5.upgrade(carA) == 0);
+        System.out.println("---Testing toString");
+        System.out.println(r1);
+        test(r1.toString().equals(r1ToString));
+        test(r2.toString().equals(r2ToString));
+        test(r3.toString().equals(r3ToString));
+        test(r4.toString().equals(r4ToString));
+        test(r5.toString().equals(r5ToString));
+        test(r6.toString().equals(r6ToString));
+        test(r7.toString().equals(r7ToString));
+        test(r8.toString().equals(r8ToString));
+    }
+
+    private static void test(boolean testCondition) {
+        System.out.println(ANSI_BLUE + "TestNum: " + (++testNumber) + ANSI_RESET);
+        if (testCondition) {
+            System.out.println(ANSI_GREEN + "OK" + ANSI_RESET);
+        } else {
+            System.out.println(ANSI_RED + "ERROR" + ANSI_RESET);
+        }
+    }
+}
