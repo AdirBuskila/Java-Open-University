6fe557ab3939f259ab0638ec84a3c07c0834fe96
Adir Buskila <Adirbu98@gmail.com>
2022-12-08 19:52:11 +0200

    updating Date

--- Date.java
+++ Date.java
@@ -33,54 +33,13 @@ public class Date {
      * 
      */
     public Date(int day, int month, int year) {
-        // checking if the values are not within the max range
-        if (day < DAY_MIN || day > DAY_MAX || month < MONTH_MIN || month > MONTH_MAX || year < YEAR_MIN
-                || year > YEAR_MAX) {
+        if (isValidDate(day, month, year)) {
+            _day = day;
+            _month = month;
+            _year = year;
+        } else {
             setDefault();
-            return;
         }
-        boolean isLeapYear = isLeapYear(year);
-        switch (month) {
-            case 2:
-                // is leap year and the day is greater than the max
-                // is not leap year and day is greater than feb max
-                if ((isLeapYear && day > FEB_LEAP_MAX_DAYS) || (!isLeapYear && day > FEB_REGULAR_MAX_DAYS)) {
-                    setDefault();
-                } else {
-                    _day = day;
-                    _month = month;
-                    _year = year;
-                }
-                break;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                // day is greater than regular month max
-                if (day > REGULAR_MONTH_MAX_DAYS) {
-                    setDefault();
-                } else {
-                    _day = day;
-                    _month = month;
-                    _year = year;
-                }
-                break;
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-            case 12:
-                // no need for checking at this point
-                // because already checked if within range at the start
-                _day = day;
-                _month = month;
-                _year = year;
-            default:
-                break;
-        }
-
     }
 
     /*
@@ -111,13 +70,7 @@ public class Date {
      * dayToSet - the day value to be set
      */
     public void setDay(int day) {
-        // creating a new date with the new given value
-        Date checkedDate = new Date(day, this.getMonth(), this.getYear());
-        // if the checkedDate values are equal to the unchanged values,
-        // and the checkedDate.day is equal to the given day,
-        // it's a valid date
-        if (day == checkedDate.getDay() && this.getMonth() == checkedDate.getMonth()
-                && this.getYear() == checkedDate.getYear()) {
+        if (isValidDate(day, _month, _year)) {
             _day = day;
         }
     }
@@ -139,13 +92,7 @@ public class Date {
      * monthToSet - the month value to be set
      */
     public void setMonth(int month) {
-        // creating a new date with the new given value
-        Date checkedDate = new Date(this.getDay(), month, this.getYear());
-        // if the checkedDate values are equal to the unchanged values,
-        // and the checkedDate.month is equal to the given month,
-        // it's a valid date
-        if (this.getDay() == checkedDate.getDay() && month == checkedDate.getMonth()
-                && this.getYear() == checkedDate.getYear()) {
+        if (isValidDate(_day, month, _year)) {
             _month = month;
         }
     }
@@ -167,13 +114,7 @@ public class Date {
      * yearToSet - the year value to be set
      */
     public void setYear(int year) {
-        // creating a new date with the new given value
-        Date checkedDate = new Date(this.getDay(), this.getMonth(), year);
-        // if the checkedDate values are equal to the unchanged values,
-        // and the checkedDate.year is equal to the given year,
-        // it's a valid date
-        if (this.getDay() == checkedDate.getDay() && this.getMonth() == checkedDate.getMonth()
-                && checkedDate.getYear() == year) {
+        if (isValidDate(_day, _month, year)) {
             _year = year;
         }
     }
@@ -261,64 +202,63 @@ public class Date {
      * the date of tomorrow
      */
     public Date tomorrow() {
-        // creating a new date with the values of the current date
-        Date tomorrow = new Date(this);
-        // calculating leap year
-        boolean isLeapYear = isLeapYear(tomorrow.getYear());
-        switch (_month) {
+        int day = _day;
+        int month = _month;
+        int year = _year;
+        day += 1;
+        if (isValidDate(day, month, year)) {
+            return new Date(day, month, year);
+        }
+        day = 1;
+        month += 1;
+        if (isValidDate(day, month, year)) {
+            return new Date(day, month, year);
+        }
+        day = 1;
+        month = 1;
+        year += 1;
+        return new Date(day, month, year);
+    }
+
+    private boolean isValidDate(int day, int month, int year) {
+        if (day < DAY_MIN || day > DAY_MAX || month < MONTH_MIN || month > MONTH_MAX || year < YEAR_MIN
+                || year > YEAR_MAX) {
+            return false;
+        }
+        boolean isLeapYear = isLeapYear(year);
+        switch (month) {
             case 2:
-                // leap year and max days: set day to min, set month to +1
-                // not leap year and max days: set day to min, set month to next month
-                if ((isLeapYear && _day == FEB_LEAP_MAX_DAYS) || (!isLeapYear && _day == FEB_REGULAR_MAX_DAYS)) {
-                    tomorrow.setDay(DAY_MIN);
-                    tomorrow.setMonth(tomorrow.getMonth() + 1);
+                // is leap year and the day is greater than the max
+                // is not leap year and day is greater than feb max
+                if ((isLeapYear && day > FEB_LEAP_MAX_DAYS) || (!isLeapYear && day > FEB_REGULAR_MAX_DAYS)) {
+                    return false;
                 } else {
-                    // next day
-                    tomorrow.setDay(tomorrow.getDay() + 1);
+                    return true;
                 }
-                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                // day is greater than regular month max
+                if (day > REGULAR_MONTH_MAX_DAYS) {
+                    return false;
+                } else {
+                    return true;
+                }
             case 1:
             case 3:
             case 5:
             case 7:
             case 8:
             case 10:
-                // day is max: set day to min, month to next month
-                if (_day == DAY_MAX) {
-                    tomorrow.setDay(DAY_MIN);
-                    tomorrow.setMonth(tomorrow.getMonth() + 1);
-                } else {
-                    // next day
-                    tomorrow.setDay(tomorrow.getDay() + 1);
-                }
-                break;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                // day is max: set day to min, month to next month
-                if (_day == REGULAR_MONTH_MAX_DAYS) {
-                    tomorrow.setDay(DAY_MIN);
-                    tomorrow.setMonth(tomorrow.getMonth() + 1);
-                } else {
-                    // next day
-                    tomorrow.setDay(tomorrow.getDay() + 1);
-                }
-                break;
             case 12:
-                // day is max: set day to min, set month to min, set year to next year
-                if (_day == DAY_MAX) {
-                    tomorrow.setDay(DAY_MIN);
-                    tomorrow.setMonth(MONTH_MIN);
-                    tomorrow.setYear(tomorrow.getYear() + 1);
-                } else {
-                    // next day
-                    tomorrow.setDay(tomorrow.getDay() + 1);
-                }
+                // no need for checking at this point
+                // because already checked if within range at the start
+                return true;
             default:
                 break;
         }
-        return tomorrow;
+        return true;
     }
 
     // sets the date to default value (1/1/2000)
