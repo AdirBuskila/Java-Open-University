4dcf46c70348d815c060a0980dbe95c82b7798b6
Adir Buskila <Adirbu98@gmail.com>
2022-12-23 18:22:40 +0200

    adding companyTest fixing company

--- AdirCompanyTest.java
+++ AdirCompanyTest.java
@@ -1,3 +1,9 @@
+/*
+ * Created by: Adir Buskila
+ * required Classes: Date, Car, Rent, Company
+ * if you find any bugs at the test please notify me :)
+ */
+
 public class AdirCompanyTest {
     private static int testNumber = 0;
 
@@ -12,19 +18,40 @@ public class AdirCompanyTest {
         Date d7 = new Date(22, 1, 2000);
         Date d8 = new Date(25, 1, 2000);
         Date d9 = new Date(29, 1, 2000);
+        Date d10 = new Date(3, 2, 2000);
+        Date d11 = new Date(10, 2, 2000);
+        Date d12 = new Date(20, 2, 2000);
         Car carA = new Car(123456789, 'A', "Ford", false);
         Car carB = new Car(123456789, 'B', "Ford", false);
         Car carC = new Car(123456789, 'C', "Ford", false);
         Car carD = new Car(123456789, 'D', "Ford", false);
         Rent r1 = new Rent("Rent 1", carA, d1, d2);
-        Rent r2 = new Rent("Rent 2", carA, d2, d3);
-        Rent r3 = new Rent("Rent 3", carA, d3, d4);
-        Rent r4 = new Rent("Rent 4", carA, d4, d5);
-        Rent r5 = new Rent("Rent 5", carA, d5, d6);
-        Rent r6 = new Rent("Rent 6", carB, d6, d7);
+        Rent r2 = new Rent("Rent 2", carB, d2, d3);
+        Rent r3 = new Rent("Rent 3", carC, d3, d4);
+        Rent r4 = new Rent("Rent 4", carC, d4, d5);
+        Rent r5 = new Rent("Rent 5", carC, d5, d6);
+        Rent r6 = new Rent("Rent 6", carC, d6, d7);
         Rent r7 = new Rent("Rent 7", carC, d7, d8);
-        Rent r8 = new Rent("Rent 8", carD, d8, d9);
+        Rent r8 = new Rent("Rent 8", carC, d8, d9);
+        Rent r10 = new Rent("Rent 10", carC, d10, d11);
+        Rent r11 = new Rent("Rent 11", carD, d11, d12);
+        String toStringTest = "The company has 11 rents:\n" +
+                "Name:Rent 1 From:01/01/2000 To:06/01/2000 Type:A Days:5 Price:500\n" +
+                "Name:Rent 2 From:06/01/2000 To:08/01/2000 Type:B Days:2 Price:300\n" +
+                "Name:Rent 3 From:08/01/2000 To:10/01/2000 Type:C Days:2 Price:360\n" +
+                "Name:Rent 4 From:10/01/2000 To:16/01/2000 Type:C Days:6 Price:1080\n" +
+                "Name:Rent 5 From:16/01/2000 To:20/01/2000 Type:C Days:4 Price:720\n" +
+                "Name:Rent 6 From:20/01/2000 To:22/01/2000 Type:C Days:2 Price:360\n" +
+                "Name:Rent 7 From:22/01/2000 To:25/01/2000 Type:C Days:3 Price:540\n" +
+                "Name:Rent 8 From:25/01/2000 To:29/01/2000 Type:C Days:4 Price:720\n" +
+                "Name:Rent 9 From:29/01/2000 To:03/02/2000 Type:C Days:5 Price:900\n" +
+                "Name:Rent 10 From:03/02/2000 To:10/02/2000 Type:C Days:7 Price:1134\n" +
+                "Name:Rent 11 From:10/02/2000 To:20/02/2000 Type:D Days:10 Price:2232";
+        System.out.println("---Testing Constructor---");
         Company c = new Company();
+        Company c1 = new Company();
+        test(c.getNumOfRents() == 0);
+        test(c.getRents().length == 1000);
         c.addRent("Rent 8", carC, d8, d9);
         c.addRent("Rent 7", carC, d7, d8);
         c.addRent("Rent 6", carC, d6, d7);
@@ -33,7 +60,82 @@ public class AdirCompanyTest {
         c.addRent("Rent 3", carC, d3, d4);
         c.addRent("Rent 2", carB, d2, d3);
         c.addRent("Rent 1", carA, d1, d2);
-        System.out.println(c);
+        System.out.println("---Testing addRent---");
+        test(c.getRents()[0].equals(r1));
+        test(c.getRents()[1].equals(r2));
+        test(c.getRents()[2].equals(r3));
+        test(c.getRents()[3].equals(r4));
+        test(c.getRents()[4].equals(r5));
+        test(c.getRents()[5].equals(r6));
+        test(c.getRents()[6].equals(r7));
+        test(c.getRents()[7].equals(r8));
+        test(c.getRents()[8] == null);
+        System.out.println("---Testing removeRent---");
+        test(c.removeRent(d2));
+        test(!c.removeRent(d2));
+        test(!c1.removeRent(d12));
+        test(c.getRents()[0].equals(r2));
+        test(c.getRents()[1].equals(r3));
+        test(c.getRents()[2].equals(r4));
+        test(c.getRents()[3].equals(r5));
+        test(c.getRents()[4].equals(r6));
+        test(c.getRents()[5].equals(r7));
+        test(c.getRents()[6].equals(r8));
+        test(c.removeRent(d9));
+        test(!c.removeRent(d9));
+        test(c.getRents()[6] == null);
+        System.out.println("---Testing getSumOfPrices---");
+        test(c.getSumOfPrices() == 3360);
+        test(c1.getSumOfPrices() == 0);
+        c.addRent("Rent 8", carC, d8, d9);
+        c.addRent("Rent 1", carA, d1, d2);
+        test(c.getSumOfPrices() == 4580);
+        System.out.println("---Testing getSumOfDays---");
+        c.removeRent(d2);
+        c.removeRent(d9);
+        test(c.getSumOfDays() == 19);
+        test(c1.getSumOfDays() == 0);
+        c.addRent("Rent 8", carC, d8, d9);
+        c.addRent("Rent 1", carA, d1, d2);
+        test(c.getSumOfDays() == 28);
+        System.out.println("---Testing averageRent---");
+        test(c1.averageRent() == 0);
+        test(c.averageRent() == 3.5);
+        c.addRent("Rent 9", carC, d9, d10);
+        test(c.averageRent() == 3.6666666666666665);
+        c.addRent("Rent 10", carC, d10, d11);
+        test(c.averageRent() == 4.0);
+        c.addRent("Rent 11", carD, d11, d12);
+        test(c.averageRent() == 4.545454545454546);
+        System.out.println("---Testing lastCarRent---");
+        test(c1.lastCarRent() == null);
+        test(c.lastCarRent().equals(carD));
+        c.removeRent(d12);
+        test(c.lastCarRent().equals(carC));
+        c.addRent("Rent 11", carD, d11, d12);
+        System.out.println("---Testing longestRent---");
+        test(c1.longestRent() == null);
+        test(c.longestRent().equals(r11));
+        c.removeRent(d12);
+        test(c.longestRent().equals(r10));
+        c.addRent("Rent 11", carD, d11, d12);
+        System.out.println("---Testing mostCommonRate---");
+        test(c.mostCommonRate() == 'C');
+        test(c1.mostCommonRate() == 'N');
+        c1.addRent("Rent 12", carD, d11, d12);
+        test(c1.mostCommonRate() == 'D');
+        c1.addRent("Rent 13", carA, d11, d12);
+        c1.addRent("Rent 14", carA, d11, d12);
+        c1.addRent("Rent 16", carA, d11, d12);
+        test(c1.mostCommonRate() == 'A');
+        System.out.println("---Testing toString---");
+        c1.removeRent(d12);
+        c1.removeRent(d12);
+        c1.removeRent(d12);
+        c1.removeRent(d12);
+        test(c1.toString() == "The company has 0 rents.");
+        test(c.toString().equals(toStringTest));
+
     }
 
     private static void test(boolean testCondition) {
