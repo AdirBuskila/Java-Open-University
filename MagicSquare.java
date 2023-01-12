934239b35f1ce0eb9feda8658b5478172edb4c75
Adir Buskila <Adirbu98@gmail.com>
2023-01-12 18:19:03 +0200

    according to the requirements

--- MagicSquare.java
+++ MagicSquare.java
@@ -1,22 +1,21 @@
 public class MagicSquare {
 
     public static int sumRow(int[][] mat, int rowNumber) {
-        int matLength = (mat.length < mat[0].length) ? mat.length : mat[0].length;
+
         int sum = 0;
         int[] row = mat[rowNumber];
-        for (int i = 0; i < matLength; i++) {
+        for (int i = 0; i < mat[rowNumber].length; i++) {
             sum += row[i];
         }
         return sum;
     }
 
     public static int sumCol(int[][] mat, int colNumber) {
-        int matLength = (mat.length < mat[0].length) ? mat.length : mat[0].length;
 
         int sum = 0;
-        for (int i = 0; i < matLength; i++) {
+        for (int i = 0; i < mat.length; i++) {
             int[] curRow = mat[i];
-            for (int j = 0; j < matLength; j++) {
+            for (int j = 0; j < mat[0].length; j++) {
                 if (j == colNumber) {
                     sum += curRow[j];
                 }
@@ -26,12 +25,11 @@ public class MagicSquare {
     }
 
     public static int sumPrimaryDiag(int[][] mat) {
-        int matLength = (mat.length < mat[0].length) ? mat.length : mat[0].length;
 
         int sum = 0;
-        for (int i = 0; i < matLength; i++) {
+        for (int i = 0; i < mat.length; i++) {
             int[] curRow = mat[i];
-            for (int j = 0; j < matLength; j++) {
+            for (int j = 0; j < mat[0].length; j++) {
                 if (i == j) {
                     sum += curRow[j];
                 }
@@ -41,13 +39,12 @@ public class MagicSquare {
     }
 
     public static int sumSecondaryDiag(int[][] mat) {
-        int matLength = (mat.length < mat[0].length) ? mat.length : mat[0].length;
 
         int sum = 0;
-        for (int i = 0; i < matLength; i++) {
+        for (int i = 0; i < mat.length; i++) {
             int[] curRow = mat[i];
-            for (int j = 0; j < matLength; j++) {
-                if (i + j == matLength - 1) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (i + j == mat.length - 1) {
                     sum += curRow[j];
                 }
             }
@@ -56,13 +53,12 @@ public class MagicSquare {
     }
 
     public static boolean isMagicSquare(int[][] mat) {
-        int matLength = (mat.length < mat[0].length) ? mat.length : mat[0].length;
 
         int sumRow;
         int sumCol;
         int sumPrimary = sumPrimaryDiag(mat);
         int sumSecondary = sumSecondaryDiag(mat);
-        for (int i = 0; i < matLength; i++) {
+        for (int i = 0; i < mat.length; i++) {
             sumRow = sumRow(mat, i);
             sumCol = sumCol(mat, i);
             if (sumRow != sumPrimary || sumSecondary != sumCol || sumPrimary != sumSecondary) {
